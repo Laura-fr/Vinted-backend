70f6976d77ed30afe0b682dbc23930c221f19e59
Laura-fr <laurafraysse84@gmail.com>
2021-01-08 15:56:17 +0100

    payment changed to give a try

--- routes/payment.js
+++ routes/payment.js
@@ -1,28 +1,26 @@
 const express = require("express");
-const formidableMiddleware = require("express-formidable");
-const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
-const app = express();
 const router = express.Router();
-app.use(formidableMiddleware());
+const createStripe = require("stripe");
+const formidableMiddleware = require("express-formidable");
+router.use(formidableMiddleware());
 
 /* Votre clé privée doit être indiquée ici */
-const stripe = createStripe();
+const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
 // on réceptionne le token
 router.post("/payment", async (req, res) => {
   try {
-    const stripeToken = req.fields.stripeToken;
     // on envoie le token a Stripe avec le montant
-    const response = await stripe.charges.create({
+    let { status } = await stripe.charges.create({
       amount: req.fields.amount * 100,
       currency: "eur",
       description: `Paiement vinted pour : ${req.fields.title}`,
-      source: stripeToken,
+      source: req.fields.token,
     });
     // Le paiement a fonctionné
     // On peut mettre à jour la base de données
     // On renvoie une réponse au client pour afficher un message de statut
-    res.json(response);
+    res.json({ status });
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ error: error.message });
